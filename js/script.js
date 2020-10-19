cecf43077cd1f3c3ecd2430691fa7a209feca6ce
William R Peters <31421828+peterswi@users.noreply.github.com>
2020-10-19 17:37:37 -0400

    rocking some basic barcharts

--- js/script.js
+++ js/script.js
@@ -12,4 +12,55 @@ d3.csv('coffee-house-chains.csv', d3.autoType).then(data=>{
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+
+    const names=[...  new Set(data.map(data=>data.company))]
+  
+    const fillColor=d3.scaleOrdinal(d3.schemeTableau10)
+    const xScale=d3.scaleBand()
+        .domain(names)
+        .rangeRound([0,width])
+        .paddingInner(0.1)
+
+    const xAxis = d3.axisBottom()
+        .scale(xScale)
+        
+    
+    const yScale=d3.scaleLinear()
+        .domain([0,d3.max(data, d=>d.stores)])
+        .range([height, 0])
+
+    const yAxis = d3.axisLeft()
+        .scale(yScale)
+        .ticks(10)
+
+    svg.selectAll('.chart')
+        .data(data)
+        .enter()
+        .append('rect')
+        .attr('x',d=>xScale(d.company))
+        .attr('y',d=>yScale(d.stores))
+        .attr('width', xScale.bandwidth())
+        .attr('height', height)
+        .style('fill',d=>fillColor(d.stores))
+        
+    svg.append("g")
+        .attr("class", "axis x-axis")
+        .attr("transform", `translate(0, ${height})`)
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "axis y-axis")
+        //.attr("transform", `translate(0, ${height})`)
+        .call(yAxis);
+
+    svg.append('text')
+        .attr('class','yaxisTitle')
+        
+        .attr('x', 0)
+        .attr('y',0 )
+        .text("Store Count")
+        .style('text-anchor','middle')
+
+    
+        
 }) 
